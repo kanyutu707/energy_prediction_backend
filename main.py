1d4b903123cf8d5fdb7122a678341267e477ac91
Alfred <165476851+kanyutu707@users.noreply.github.com>
2025-04-25 20:23:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Energy Consumption Prediction API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://energyprediction.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
